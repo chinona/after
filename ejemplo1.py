8d0998cd1f8c1b499ca69c614c4042dd0fe71890
agustin carranza <agustincarranzacarabajal@gmail.com>
2024-07-04 15:52:10 -0300

    ultima version

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,2 +1,5 @@
-def mensaje():
+def mensaje1():
     print("mi primera version")
+
+def mensaje3():
+    print("mi mensaje 3")
---
+++ ejemplo2.py
@@ -0,0 +1,5 @@
+def mensaje2():
+    print("estoy trabajando con git")
+
+def mensaje4():
+    print("mi primera version")
